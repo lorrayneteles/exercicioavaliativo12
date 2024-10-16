f9db5be1d2c429ceb4c717a5dff6656d96920fff
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 18:37:03 -0300

    Add files via upload

---
+++ exercicioavaliativo12.py
@@ -0,0 +1,9 @@
+#Área de um triângulo
+
+base = int(input("Digite o valor da base do triângulo:"))
+
+altura = int(input("Digite o valor da altura do triângulo:"))
+
+area = (base * altura)/2
+
+print(f"O valor da área do triângulo é:", area)
